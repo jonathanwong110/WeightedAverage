97b46f7d72a76217cb111117fdbf060f2025265a
jonathanwong110 <jonathan.wong110@gmail.com>
2023-05-23 14:00:51 -0400

    assignments are now viewable on the home page and there is a separate page for each assignment now

--- weightedaverage/myweightedaverage/views.py
+++ weightedaverage/myweightedaverage/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Assignment
 
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Assignment
+    template_name = 'home.html'
+
+
+class AssignmentDetailView(DetailView):
+    model = Assignment
+    template_name = 'assignment_details.html'
