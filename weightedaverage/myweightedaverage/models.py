a0c337dae79cf6927c558d44bb5beb3c0963714e
jonathanwong110 <jonathan.wong110@gmail.com>
2023-05-23 12:11:32 -0400

    created an assignment class

--- weightedaverage/myweightedaverage/models.py
+++ weightedaverage/myweightedaverage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+TASK_CHOICES = (
+    ('task', 'TASK'),
+    ('quiz', 'QUIZ'),
+    ('exam', 'EXAM'),
+)
+
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=255)
+    task = models.CharField(max_length=4, choices=TASK_CHOICES, default="task")
+    grade = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.name + ' | ' + self.task + ' | ' + str(self.grade) + ' | ' + str(self.weight)
